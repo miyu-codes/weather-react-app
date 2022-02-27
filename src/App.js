c28d31c2ca1326d58cf3db4565c1f49da9ef463b
miyu-codes <96804318+miyu-codes@users.noreply.github.com>
2022-02-27 11:48:29 +0900

    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather Browser</h1>
-      <p>
-        <a
-          href="https://github.com/miyu-codes/weather-react-app"
-          target="_blank"
-        >
-          Open-source code
-        </a>{" "}
-        by Miyu Hayakawa
-      </p>
+      <div className="container">
+        <h1>Weather Browser</h1>
+        <footer>
+          <a
+            href="https://github.com/miyu-codes/weather-react-app"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Miyu Hayakawa
+        </footer>
+      </div>
     </div>
   );
 }
